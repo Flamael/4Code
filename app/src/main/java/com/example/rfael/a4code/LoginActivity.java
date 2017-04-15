ccad7b77bed6216b675a82b2e16d97573bc5b830
Flamael <flamaell@hotmail.com>
2017-04-15 10:17:13 -0300

    Telas de Cadastro e Login.

--- app/src/main/java/com/example/rfael/a4code/LoginActivity.java
+++ app/src/main/java/com/example/rfael/a4code/LoginActivity.java
@@ -6,30 +6,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 
-public class ApresentacaoActivity extends ActionBarActivity {
-
-    Button criarConta, entrar;
+public class LoginActivity extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.apresentacao);
-
-        criarConta = (Button) findViewById(R.id.criarConta);
-        entrar = (Button) findViewById(R.id.entrar);
+        setContentView(R.layout.login);
 
     }
 
-    public void criarConta(View view) {
-        startActivity(new Intent(this, CadastroActivity.class));
+    public void logar(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 
-    public void entrar (View view) {
-        startActivity(new Intent(this, LoginActivity.class));
+    public void voltar(View view) {
+        startActivity(new Intent(this, ApresentacaoActivity.class));
     }
 
 }
