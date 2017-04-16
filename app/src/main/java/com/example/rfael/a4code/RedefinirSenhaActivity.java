78cf1f26b972ca897fcffc70f13fe23491df8029
Flamael <flamaell@hotmail.com>
2017-04-16 15:22:01 -0300

    Activity de redefinição de senha.

--- app/src/main/java/com/example/rfael/a4code/RedefinirSenhaActivity.java
+++ app/src/main/java/com/example/rfael/a4code/RedefinirSenhaActivity.java
@@ -7,27 +7,23 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class LoginActivity extends ActionBarActivity{
+public class RedefinirSenhaActivity extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.login);
+        setContentView(R.layout.redefinir_senha);
 
-    }
-
-    public void logar(View view) {
-        startActivity(new Intent(this, MainActivity.class));
     }
 
     public void voltar(View view) {
-        startActivity(new Intent(this, ApresentacaoActivity.class));
+        startActivity(new Intent(this, LoginActivity.class));
     }
 
-    public void redefinirSenha(View view) {
-        startActivity(new Intent(this, RedefinirSenhaActivity.class));
+    public void enviar(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
     }
 
 }
