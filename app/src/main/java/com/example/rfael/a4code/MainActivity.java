836e85f89163684d62713e6e430326fd6c8c138a
Flamael <flamaell@hotmail.com>
2017-04-18 00:47:53 -0300

    Melhoramento nos layouts

--- app/src/main/java/com/example/rfael/a4code/MainActivity.java
+++ app/src/main/java/com/example/rfael/a4code/MainActivity.java
@@ -74,13 +74,13 @@ public class MainActivity extends ActionBarActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.usuario) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.configuracao) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.compartilhar) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.logout) {
 
         }
 
